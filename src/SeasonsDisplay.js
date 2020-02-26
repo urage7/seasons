1d270b470a320e75102dd61045d9e6608143765e
urage7 <53519075+urage7@users.noreply.github.com>
2020-02-26 09:05:33 +0200

    asd
    asd

---
+++ src/SeasonsDisplay.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const seasonConfig = {
+  summer: {
+    text: "Lets hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr, its chilly",
+    iconName: "snowflake"
+  }
+};
+
+const getSesason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonsDisplay = props => {
+  const season = getSesason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season]; //{text, iconName}
+  return (
+    <div>
+      <i className={`massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`massive ${iconName} icon`} />
+    </div>
+  );
+};
+
+export default SeasonsDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonsDisplay from "./SeasonsDisplay";
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  //   componentDidUpdate() {
+  //     console.log("My component just was updated");
+  //   }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonsDisplay lat={this.state.lat} />;
+    }
+    return <div>Loading!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
